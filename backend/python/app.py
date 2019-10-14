97c9cf1b21c4cf1ac70f3254205b27f970594811
Eric Zhao <eric.zhao306@gmail.com>
2019-10-14 21:06:34 +1100

    add graphql resolver

--- backend/python/app.py
+++ backend/python/app.py
@@ -1,4 +1,6 @@
 from graphene import ObjectType, String, Schema
+from flask import Flask
+from flask_graphql import GraphQLView
 
 class Query(ObjectType):
     # this defines a Field `hello` in our Schema with a single Argument `name`
@@ -16,14 +18,33 @@ class Query(ObjectType):
 schema = Schema(query=Query)
 
 # we can query for our field (with the default argument)
-query_string = '{ hello }'
-result = schema.execute(query_string)
-print(result.data['hello'])
-# "Hello stranger"
+# query_string = '{ hello }'
+# result = schema.execute(query_string)
+# print(result.data['hello'])
+# # "Hello stranger"
 
-# or passing the argument in the query
-query_with_argument = '{ hello(name: "GraphQL") }'
-result = schema.execute(query_with_argument)
-print(result.data['hello'])
+# # or passing the argument in the query
+# query_with_argument = '{ hello(name: "GraphQL") }'
+# result = schema.execute(query_with_argument)
+# print(result.data['hello'])
 # "Hello GraphQL!"
 
+
+app = Flask(__name__)
+app.debug = True
+
+# Routes
+app.add_url_rule(
+    '/graphql', view_func=GraphQLView.as_view(
+        'graphql',
+        schema=schema,
+        graphiql=True
+    )
+)
+
+@app.route('/')
+def index():
+    return '<p> Hello World </p>'
+
+if __name__ == '__main__':
+    app.run()
